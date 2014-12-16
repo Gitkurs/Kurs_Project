453598c3bde634a6188f695bdd8c4042467f4023
SchwarzAnton <notna_94@bk.ru>
2014-12-17 00:04:04 +0300

    add mysql_escape_string

--- action.php
+++ action.php
@@ -1,12 +1,11 @@
 <?
 include "connect.php";
 
-$name = $_POST['name'];
-
-$surname = $_POST['surname'];
-$email = $_POST['email'];
-$login = $_POST['login'];
-$password = $_POST['password'];
+$name = mysql_escape_string($_POST['name']);
+$surname = mysql_escape_string($_POST['surname']);
+$email = mysql_escape_string($_POST['email']);
+$login = mysql_escape_string($_POST['login']);
+$password = mysql_escape_string($_POST['password']);
 
 $query = mysql_query("INSERT INTO users (name,surname,email,password) VALUES ('$name','$surname','$email','$password')") or die(mysql_error());
 
